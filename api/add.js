f6f1a6758e82facc53b9ce9e63515a0f0846fc55
boris.loor06 <boris.loor06@gmail.com>
2023-01-16 21:34:29 -0500

    estado inicial

---
+++ api/add.js
@@ -0,0 +1,13 @@
+import { connectToDatabase } from "../../lib/db";
+
+export default async function handler(request, response) {
+    
+    const { text, encrypt } = request.body;
+    const { database } = await connectToDatabase();
+    const collection = database.collection(process.env.NEXT_ATLAS_COLLECTION);
+
+    const results = await collection.insertOne({textoPlano: text, textoCifrado: encrypt});
+
+    response.status(200).json(results);
+
+}
